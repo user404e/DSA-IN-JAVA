c60c85ee5b1ba810c670f0ce0e4c6964caa8e5a6
user404e <e404user@gmail.com>
2023-05-17 08:44:49 +0000

    DSA-JAVA

---
+++ ARRAY/TRAVERSAL/TraverseArray.java
@@ -0,0 +1,9 @@
+public class TraverseArray {
+    public static void main(String[] args) {
+        int[] a1 = {43,2,5,53,23,55,3,35};
+
+        for(int i=0;i<a1.length;i++){
+            System.out.println("a1[" + i + "] = " + a1[i]);
+        }
+    }
+}
